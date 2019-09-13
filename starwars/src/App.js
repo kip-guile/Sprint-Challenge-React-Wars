bfecf8f8ef9d05c18fc5c413076eb61b3398faee
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-13 09:01:57 +0100

    axios working, information redering on page. Error messahe not fixed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import Person from './components/Persons';
 
-const starwarsApi = 'https://swapi.co/api/people/';
+const starWarsApi = 'https://swapi.co/api/people/';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,11 +14,27 @@ const App = () => {
   // sync up with, if any.
 
   const [starWarsState, setStarWarsSate] = useState([]);
+  const [count, setCount] = React.useState(1)
+
+  useEffect(() => {
+    axios.get(starWarsApi)
+      .then(response => {
+        const block = response.data.results;
+        setStarWarsSate(block);
+        setCount(2);
+        console.log(starWarsState);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }, [count]);
 
   return (
+    starWarsState.map(person =>
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Person name = {person.name} height = {person.height} mass = {person.mass} dob = {person.birth_year} gender ={person.gender} hair = {person.hair_color} skin = {person.skin_color} eye = {person.eye_color}/>
     </div>
+    )
   );
 }
 
